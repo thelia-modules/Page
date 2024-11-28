3d2177161d74c9203d35520a59e55d076328d814
Lucanis <damien.foulhoux@gmail.com>
2024-11-28 11:17:06 +0100

    twig compatibility

--- EventListener/KernelViewListener.php
+++ EventListener/KernelViewListener.php
@@ -2,35 +2,41 @@
 
 namespace Page\EventListener;
 
+use Page\Model\Map\PageTableMap;
 use Page\Model\PageQuery;
 use Page\Service\PageService;
 use Propel\Runtime\ActiveQuery\Criteria;
+use Propel\Runtime\ActiveQuery\Join;
 use Propel\Runtime\Exception\PropelException;
 use SmartyException;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpKernel\Event\ViewEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
+use Thelia\Core\Template\Parser\ParserResolver;
+use Thelia\Core\Template\ParserInterface;
 use Thelia\Core\Template\TemplateHelperInterface;
+use Thelia\Model\Tools\ModelCriteriaTools;
+use TheliaBlocks\Model\Map\BlockGroupI18nTableMap;
+use TheliaBlocks\Model\Map\ItemBlockGroupTableMap;
+use TheliaBlocks\Service\JsonBlockService;
+use TheliaBlocks\TheliaBlocks;
 use TheliaSmarty\Template\SmartyParser;
 
 class KernelViewListener implements EventSubscriberInterface
 {
-    protected TemplateHelperInterface $templateHelper;
-    protected SmartyParser $parser;
-    protected RequestStack $requestStack;
-    protected PageService $pageService;
+
+    protected ParserInterface  $parser;
 
     public function __construct(
-        RequestStack  $requestStack,
-        SmartyParser  $parser,
-        TemplateHelperInterface $templateHelper,
-        PageService $pageService
+        protected RequestStack  $requestStack,
+        protected ParserResolver  $parserResolver,
+        protected TemplateHelperInterface $templateHelper,
+        protected PageService $pageService,
+        protected JsonBlockService $jsonBlockService
     ) {
-        $this->requestStack = $requestStack;
-        $this->parser = $parser;
-        $this->templateHelper = $templateHelper;
-        $this->pageService = $pageService;
+        
+        
     }
 
     /**
@@ -39,26 +45,80 @@ class KernelViewListener implements EventSubscriberInterface
      */
     public function onKernelView(ViewEvent $event)
     {
-        $currentTemplateDefinition = $this->parser->getTemplateDefinition();
-        $currentFallbackToDefaultTemplate = $this->parser->getFallbackToDefaultTemplate();
-
-        $this->parser->setTemplateDefinition($this->templateHelper->getActiveFrontTemplate(), true);
-
         $request = $this->requestStack->getCurrentRequest();
         $view = $request->attributes->get('_view');
         $viewId = $request->attributes->get($view . '_id');
+
+ 
+        
 
         if ($view !== 'page' && $view !== "index") {
             return;
         }
 
+        $pageQuery = PageQuery::create();
+        
+        ModelCriteriaTools::getI18n(
+            false,
+            $this->requestStack->getCurrentRequest()?->getSession()?->getLang()?->getId(),
+            $pageQuery,
+            $this->requestStack->getCurrentRequest()?->getSession()?->getLang()?->getLocale(),
+            ['TITLE', 'CHAPO', 'DESCRIPTION', 'POSTSCRIPTUM', 'META_TITLE', 'META_DESCRIPTION', 'META_KEYWORDS'],
+            null,
+            "ID",
+            true
+        );
+
+        $joinItemBlockGroup = new Join();
+        $joinItemBlockGroup->setJoinType(Criteria::LEFT_JOIN);
+        $joinItemBlockGroup->addExplicitCondition(
+            PageTableMap::TABLE_NAME,
+            'ID',
+            null,
+            ItemBlockGroupTableMap::TABLE_NAME,
+            'ITEM_ID',
+            null
+        );
+        $pageQuery->addJoinObject($joinItemBlockGroup, 'item_block_group')
+            ->addJoinCondition(
+                'item_block_group',
+                '`item_block_group`.`item_type` = ?',
+                'page',
+                null,
+                \PDO::PARAM_STR
+            );
+        $pageQuery->withColumn('item_block_group.block_group_id', 'block_group_id');
+
+        $locale = $this->requestStack->getCurrentRequest()->getSession()->getLang()->getLocale();
+        $joinBlockGroupI18n = new Join();
+        $joinBlockGroupI18n->setJoinType(Criteria::LEFT_JOIN);
+        $joinBlockGroupI18n->addExplicitCondition(
+            ItemBlockGroupTableMap::TABLE_NAME,
+            'BLOCK_GROUP_ID',
+            null,
+            BlockGroupI18nTableMap::TABLE_NAME,
+            'ID',
+            null
+        );
+
+        $pageQuery->addJoinObject($joinBlockGroupI18n, 'block_group_i18n')
+            ->addJoinCondition(
+                'block_group_i18n',
+                '`block_group_i18n`.`locale` = ?',
+                $locale,
+                null,
+                \PDO::PARAM_STR
+            );
+        $pageQuery->withColumn('block_group_i18n.title', 'block_group_title');
+        $pageQuery->withColumn('block_group_i18n.json_content', 'block_group_content');
+
         if ($viewId) {
-            $page = PageQuery::create()->filterById($viewId)
+            $page = $pageQuery->filterById($viewId)
             ->findOne();
         }
 
         if ($view === "index") {
-            $page = PageQuery::create()
+            $page = $pageQuery
             ->filterByIsHome(1)
             ->findOne();
         }
@@ -67,6 +127,20 @@ class KernelViewListener implements EventSubscriberInterface
         if (!$page) {
             return;
         }
+        
+
+
+        $path = $this->templateHelper->getActiveFrontTemplate()->getAbsolutePath();
+        $this->parser = $this->parserResolver->getParser($path, $view);
+        // Define the template that should be used
+        $this->parser->setTemplateDefinition(
+            $this->parser->getTemplateDefinition() ?: $this->templateHelper->getActiveFrontTemplate()
+        );
+
+
+        $currentTemplateDefinition = $this->parser->getTemplateDefinition();
+        $currentFallbackToDefaultTemplate = $this->parser->getFallbackToDefaultTemplate();
+
 
         $view = $this->pageService->getPageTemplateName($page);
 
@@ -74,7 +148,12 @@ class KernelViewListener implements EventSubscriberInterface
             $request->attributes->set('_view', $view);
         }
 
-        $page->setLocale($this->requestStack->getCurrentRequest()->getSession()->getLang()->getLocale());
+        
+        $page->setLocale($this->requestStack->getCurrentRequest()?->getSession()?->getLang()?->getLocale());
+        
+        $htmlRender = $this->jsonBlockService->renderJsonBlocks($page->getVirtualColumn('block_group_content'));
+        
+        $this->parser->assign('pageHtmlRender', $htmlRender);
         $this->parser->assign('page', $page);
 
         if ($currentTemplateDefinition) {
