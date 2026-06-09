45d2ac9382d390b708d519428c3efeed097098bb
Alexandre Nozière <anoziere@openstudio.fr>
2026-06-09 21:28:35 +0200

    feat: migrate and modernize back-office hook for Twig

--- Hook/PageMenuHook.php
+++ Hook/PageMenuHook.php
@@ -22,7 +22,7 @@ class PageMenuHook extends BaseHook
     public function onMainInTopMenuItems(HookRenderEvent $event): void
     {
         $event->add(
-            $this->render('hook/hook-in-top-menu-item.html', $event->getTemplateVars())
+            $this->render('Page/hook/hook-in-top-menu-item.html.twig', $event->getTemplateVars())
         );
     }
 }
